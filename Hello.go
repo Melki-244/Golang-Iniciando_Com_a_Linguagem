0d505374087b71bbf551d3c0c0c529e6378431f7
Melki-244 <melkicavalcante244@gmail.com>
2022-05-06 15:18:59 -0300

    Trabalhando com Variáveis| Tipos das Variáveis

--- Hello.go
+++ Hello.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main()  {
-   fmt.Println("Hello World!") 
+   var nome string = "Melki"
+   var idade int = 20
+   var versao float32 = 1.1
+   fmt.Println("Olá sr.", nome, "Sua Idade é", idade) 
+   fmt.Println("Este Programa Está Na Versão", versao)
 }
