e0124000c64a7f62c110ffe0e1c5b0a889f63e3a
Melki-244 <melkicavalcante244@gmail.com>
2022-05-07 18:58:35 -0300

    Controlando Fluxo do Script | Controle de Fluxo com If

--- Hello.go
+++ Hello.go
@@ -14,6 +14,15 @@ func main()  {
    
    var comando int
    fmt.Scan(&comando)
-   
    fmt.Println("O comando Escolhido Foi", comando)
+
+   if comando == 1 {
+     fmt.Println("Iniciando Monitoramento") 
+   }else if comando == 2 {
+     fmt.Println("Exibindo Logs")
+   }else if comando == 0 {
+     fmt.Println("Saindo Do Programa")
+   }else{
+     fmt.Println("Não Conheço Seu Comando")
+   }
 }
