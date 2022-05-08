b2343292a449de7d262b71459d4ecb3704ca4ffb
Melki-244 <melkicavalcante244@gmail.com>
2022-05-08 11:04:57 -0300

    Fazendo Requisições Para Web | Monitorando Nossos Sites <Iniciando Com Loops {for}>

--- Hello.go
+++ Hello.go
@@ -7,24 +7,26 @@ import "net/http"
 func main()  {
    introducao()   
 
-   fmt.Println("1- Iniciar Monitoramento")
-   fmt.Println("2- Exibir os Logs")
-   fmt.Println("0- Sair do Programa")
-   
-   comando := leComando()
+   for {
+     fmt.Println("1- Iniciar Monitoramento")
+     fmt.Println("2- Exibir os Logs")
+     fmt.Println("0- Sair do Programa")
+     
+     comando := leComando()
 
-   switch comando {
-     case 1:
-       iniciarMonitoramento()
-     case 2:
-       fmt.Println("Exibindo Logs")   
-     case 0:
-       fmt.Println("Saindo do Programa") 
-       os.Exit(0) 
-     default:
-       fmt.Println("Não Conheço Esse Programa")
-       os.Exit(-1)
-   }
+     switch comando {
+       case 1:
+         iniciarMonitoramento()
+       case 2:
+         fmt.Println("Exibindo Logs")   
+       case 0:
+         fmt.Println("Saindo do Programa") 
+         os.Exit(0) 
+       default:
+         fmt.Println("Não Conheço Esse Programa")
+         os.Exit(-1)
+     }
+   }   
 }
 func introducao()  {
    nome := "Melki"
@@ -35,23 +37,18 @@ func introducao()  {
 func leComando() int {
    var comandoLido int
    fmt.Scan(&comandoLido)
-   fmt.Println("O comando Escolhido Foi", comandoLido)
+   fmt.Println("O Comando Escolhido Foi", comandoLido)
    
-   return comandoLido
+   return comandoLido 
 }
 func iniciarMonitoramento()  {
   fmt.Println("Monitorando...")  
   site := "https://alura.com.br" 
-  http.Get(site)
+  resp, _ := http.Get(site)
+  
+  if resp.StatusCode == 200 {
+    fmt.Println("Site", site, "Carregado Com Sucesso!") 
+  }else{
+    fmt.Println("Site", site, "Não Foi Carregado Com Sucesso. Status Code:",resp.StatusCode)
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
