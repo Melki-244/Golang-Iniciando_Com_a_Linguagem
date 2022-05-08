7343abd89c11bdad56227affa20bc987cf771fac
Melki-244 <melkicavalcante244@gmail.com>
2022-05-07 22:04:53 -0300

    Controlando Fluxo do Script | Introdução às Funções

--- Hello.go
+++ Hello.go
@@ -1,21 +1,17 @@
 package main
 
 import "fmt"
+import "os"
 
 func main()  {
-   nome := "Melki"
-   versao := 1.1
-   fmt.Println("Olá sr.", nome) 
-   fmt.Println("Este Programa Está Na Versão", versao)
-   
+   introducao()   
+
    fmt.Println("1- Iniciar Monitoramento")
    fmt.Println("2- Exibir os Logs")
    fmt.Println("0- Sair do Programa")
    
-   var comando int
-   fmt.Scan(&comando)
-   fmt.Println("O comando Escolhido Foi", comando)
-  
+   comando := leComando()
+
    switch comando {
      case 1:
        fmt.Println("Monitorando...")  
@@ -23,10 +19,26 @@ func main()  {
        fmt.Println("Exibindo Logs")   
      case 0:
        fmt.Println("Saindo do Programa") 
+       os.Exit(0) 
      default:
        fmt.Println("Não Conheço Esse Programa")
+       os.Exit(-1)
    }
 }
+func introducao()  {
+   nome := "Melki"
+   versao := 1.1
+   fmt.Println("Olá sr.", nome) 
+   fmt.Println("Este Programa Está Na Versão", versao)
+}
+func leComando() int {
+   var comandoLido int
+   fmt.Scan(&comandoLido)
+   fmt.Println("O comando Escolhido Foi", comandoLido)
+   
+   return comandoLido
+}
+
 
 
 
