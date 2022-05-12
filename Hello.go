c11dcafc942035175a6235a63592a22f7429858a
Melki-244 <melkicavalcante244@gmail.com>
2022-05-12 15:45:02 -0300

    Escrevendo Arquivos | Formatando Tempo Em Go

--- Hello.go
+++ Hello.go
@@ -130,5 +130,5 @@ func registraLog(site string, status bool)  {
     fmt.Println("Ocorreu Um Erro Ao Abrir O Arquivo: ", err) 
   }
 
-  arquivo.WriteString(site + statusString + strconv.FormatBool(status) + "\n")
+  arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05 - ") + site + statusString + strconv.FormatBool(status) + "\n")
 }
